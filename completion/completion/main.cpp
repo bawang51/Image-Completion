1bdad63e04a75d7165be93d9bdc7d84eba9a12bb
lmq <bawang51@sina.cn>
2016-06-14 20:57:09 +0800

    code v0.1

---
+++ completion/completion/main.cpp
@@ -0,0 +1,26 @@
+#define _USE_MATH_DEFINES
+
+#include <string>
+#include <opencv2\opencv.hpp>
+#include <vector>
+#include <ctime>
+#include <math.h>
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include "bp.h"
+
+using namespace std;
+using namespace cv;
+
+int main(){
+	Mat input = imread("test.jpg");
+	Mat mask = imread("mask.jpg");
+	input.convertTo(input, CV_8UC3);
+	mask.convertTo(mask, CV_8UC3);
+	Mat res = complete();
+	imshow("result", res);
+	waitKey();
+	return 0;
+}
